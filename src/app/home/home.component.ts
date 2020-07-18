090726d5186cc522234102265e42dec8c12fee76
Brooklyn Pedley <brooklynpedley@gmail.com>
2020-07-18 17:02:39 +0100

    setup data arrays

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public pasta = [];
+  public sauces = [];
+  public meats = [];
+  public sides = [];
+
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getPastaShapes().then((res: Array<string>) => {
+      this.pasta = res;
+    });
+    this.data.getSauces().then((res: Array<string>) => {
+      this.sauces = res;
+    });
+    this.data.getMeats().then((res: Array<string>) => {
+      this.meats = res;
+    });
+    this.data.getSides().then((res: Array<string>) => {
+      this.sides = res;
+    });
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,5 +5,39 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
+  private pastaShapes = ['Capelli', 'Macaroni', 'Shells', 'Rigatoni', 'Bow-Ties',
+    'Lasagne', 'Lasagne Squares', 'Linguine', 'Parpadelle', 'Spaghetti', 'Macaroni',
+    'Fettuccine', 'Spiralli', 'Penne', 'Fussili', 'Tagliatelle'];
+  private meats = ['Chicken Breast', 'Turkey Breast', 'Beef Meatballs', 'Swedish Meatballs',
+    'Chicken Meatballs', 'Chicken Sausage', 'Sausage', 'Hotdog', 'Mince Beef', 'Decased Sausage',
+    'None'];
+  private sauces = ['Marinara', 'Alfredo', 'Arabiata', 'Mushroom', 'Green Pesto',
+    'Red Pesto', 'Bechemel', 'Chicken Tonight', 'Pink Sauce', 'Cheese Sauce',
+    'Vodka Sauce', 'Marscapone', 'Pasta Bake', 'Carbonara',];
+  private sides = ['Boiled Veg', 'Garlic Bread', 'Buttered Bread', 'Dough Balls', 'Mozerlla Sticks', 'Wedges'];
+
   constructor() { }
+
+  getPastaShapes() {
+    return new Promise((resolve) => {
+      resolve(this.pastaShapes);
+    });
+  }
+  getMeats() {
+    return new Promise((resolve) => {
+      resolve(this.meats);
+    });
+  }
+
+  getSauces() {
+    return new Promise((resolve) => {
+      resolve(this.sauces);
+    });
+  }
+
+  getSides() {
+    return new Promise((resolve) => {
+      resolve(this.sides);
+    });
+  }
 }
